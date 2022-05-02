7895a9e805e6df5534207e086eb06ee9b20eb997
Metamorg <egoronokasd@gmail.com>
2022-05-02 19:40:42 +0300

    add: add some tasks

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l,k) {
+  let current = l;
+  let prev = null;
+    while (current) {
+      if (current.value !== k) {
+        prev = current;
+        current = current.next;
+      } else {
+        if (prev === null) {
+        l = l.next;
+        current = l;
+      } else {
+        prev.next = prev.next.next;
+        current = current.next;
+      }
+    }
+  }
+  return l
 }
 
 module.exports = {
